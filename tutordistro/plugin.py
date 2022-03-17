38e98f875ced8b83a50a2c8aa672dd3e98cc1e2a
Alejandro Cardenas <alecar.main@gmail.com>
2022-03-17 11:13:02 -0500

    feat: add eox-core settings

--- tutordistro/plugin.py
+++ tutordistro/plugin.py
@@ -7,7 +7,28 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 templates = os.path.join(HERE, "templates")
 
-config = {}
+config = {
+    "defaults": {
+        "VERSION": __version__,
+        "EOX_CORE": True,
+
+        # DISTRO PACKAGES
+        "EOX_CORE_DPKG": {
+            "name": "eox-core",
+            "index": "git",
+            "repository": "https://github.com/eduNEXT/eox-core.git",
+            "version": "v6.0.1",
+            "variables": {
+                "development": {
+                    "EOX_CORE_USERS_BACKEND": "eox_core.edxapp_wrapper.backends.users_m_v1",
+                    "EOX_CORE_ENROLLMENT_BACKEND": "eox_core.edxapp_wrapper.backends.enrollment_l_v1",
+                    "EOX_CORE_PRE_ENROLLMENT_BACKEND": "eox_core.edxapp_wrapper.backends.pre_enrollment_l_v1"
+                },
+                "production": {}
+            }
+        }
+    }
+}
 
 hooks = {}
 
