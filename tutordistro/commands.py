2325a074a3155dd4d26569afdbf64c579b9e2c83
black-redoc <joseb.twelve@gmail.com>
2022-03-19 10:27:46 -0500

    feat: added distro_theme_volumes to config

--- tutordistro/commands.py
+++ tutordistro/commands.py
@@ -6,18 +6,24 @@ from tutor import config as tutor_config
 @click.pass_obj
 def enable_theme_volumes(context) -> None:
     tutor_dir = str(context.root)
-    theme_dir = "env/build/openedx/distro-themes/ednx-saas-themes"
-    click.echo(f"creating {tutor_dir}/{theme_dir}...")
-    subprocess.call(["mkdir", "-p", f"{tutor_dir}/{theme_dir}"])
+    config = tutor_config.load(context.root)
 
-    click.echo(f"clonning..")
-    subprocess.call([
-        "git",
-        "clone",
-        "-b",
-        "edunext/mango.master",
-        f"git@github.com:eduNEXT/ednx-saas-themes.git",
-        f"{tutor_dir}/{theme_dir}"
-    ])
-    click.echo(f"finishing...")
+    for theme in config["DISTRO_THEME_VOLUMES"]:
+        theme_dir = theme["src"]
+        theme_repo = theme["repository"]
+        theme_branch = theme["branch"]
+
+        click.echo(f"creating {tutor_dir}/{theme_dir}...")
+        subprocess.call(["mkdir", "-p", f"{tutor_dir}/{theme_dir}"])
+
+        click.echo(f"clonning...")
+        subprocess.call([
+            "git",
+            "clone",
+            "-b",
+            theme_branch,
+            theme_repo,
+            f"{tutor_dir}/{theme_dir}"
+        ])
+    click.echo("finishing...")
     click.echo("Theme volumes are enable now.")
