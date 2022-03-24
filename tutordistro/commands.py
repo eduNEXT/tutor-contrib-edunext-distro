eba81aae83a4f69d5b241bab4488cc03357f307a
black-redoc <joseb.twelve@gmail.com>
2022-03-24 16:36:38 -0500

    fix: added theme name to the git clone command

--- tutordistro/commands.py
+++ tutordistro/commands.py
@@ -1,5 +1,7 @@
-import click
+import os
 import subprocess
+
+import click
 from tutor import config as tutor_config
 from tutor.commands.context import Context
 
@@ -23,8 +25,11 @@ def enable_themes(context: Context) -> None:
 
         click.echo(f"clonning...")
         result = subprocess.call(
-            ["git", "clone", "-b", theme_branch, theme_repo, f"{tutor_dir}/{theme_dir}"]
+            ["git", "clone", "-b", theme_branch, theme_repo, f"{tutor_dir}/{theme_dir}/{theme['repo']}"]
         )
     if result == 0:
         click.echo("finishing...")
         click.echo("Themes are enable now.")
+    else:
+        click.echo(f"Finish not success. Error {result}")
+        click.echo("There are a trouble to enable themes.")
