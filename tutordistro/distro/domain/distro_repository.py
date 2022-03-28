3191a539e570143c8d0a502f36aee6edaf907e55
black-redoc <joseb.twelve@gmail.com>
2022-03-28 17:39:34 -0500

    refactor: added functionalities to distro_git_repository

--- tutordistro/distro/domain/distro_repository.py
+++ tutordistro/distro/domain/distro_repository.py
@@ -1,8 +1,32 @@
 from abc import ABC, abstractmethod
 
+
 class DistroRepository(ABC):
     @abstractmethod
-    def clone(self):
+    def __init__(self, theme_settings, subprocess, click):
+        """
+        __init__ method
+        Args:
+            theme_settings (ThemeSettings): Domain attributes to manage theme settings
+            subprocess (subprocess)
+            click (click)
+        """
+        pass
+
+    @abstractmethod
+    def clone(self, theme_settings) -> None:
         """
         Method to clone themes
+        """
+
+    @abstractmethod
+    def check_directory(self, os) -> None:
+        """
+        Method to check if theme directory already exists
+        """
+
+    @abstractmethod
+    def create_directory(self) -> None:
+        """
+        Method to clean old theme directory
         """
--- tutordistro/distro/infraestructure/distro_git_repository.py
+++ tutordistro/distro/infraestructure/distro_git_repository.py
@@ -1,9 +1,50 @@
 from tutordistro.distro.domain.distro_repository import DistroRepository
+from tutordistro.distro.domain.git_clone_exception import GitCloneException
 
 
 class DistroGitRepository(DistroRepository):
-    def __init__(self):
-        pass
+    def __init__(self, theme_settings, subprocess, click):
+        super.__init__(theme_settings, subprocess, click)
+        self.subprocess = subprocess
+        self.click = click
+        self.theme_settings = theme_settings
 
-    def clone(self):
-        pass
+    def clone(self) -> None:
+        super().clone()
+        self.click.echo(f"Clonning...")
+        result = self.subprocess.call(
+            [
+                "git",
+                "clone",
+                "-b",
+                self.theme_settings.branch,
+                self.theme_settings.repo,
+                f"{self.theme_settings.get_full_directory}/{self.theme_settings.repo_name}",
+            ]
+        )
+
+        if result == 0:
+            self.click.echo("Finishing...")
+            self.click.echo("Themes are enable now.")
+        else:
+            raise GitCloneException(
+                f"""
+                Finish not success. Error `subprocess api` {result}
+                There are a trouble to enable themes.
+                """
+            )
+
+    def check_directory(self, os) -> None:
+        super().check_directory(os)
+        if os.path.isdir(f"{self.theme_settings.get_full_directory}"):
+            self.click.echo("Cleaning old themes...")
+            self.subprocess.call(
+                ["sudo", "rm", "-rf", f"{self.theme_settings.get_full_directory}"]
+            )
+
+    def create_directory(self) -> None:
+        super().create_directory()
+        self.click.echo(f"Creating {self.theme_settings.get_full_directory}...")
+        self.subprocess.call(
+            ["mkdir", "-p", f"{self.theme_settings.get_full_directory}"]
+        )
