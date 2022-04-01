72d5677000876a9ffae9d3c69fa14e053e8d52a9
Alejandro Cardenas <alecar.main@gmail.com>
2022-04-01 12:05:51 -0500

    docs: update README (#4)

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 
 def load_readme():
-    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+    with io.open(os.path.join(HERE, "README.md"), "rt", encoding="utf8") as f:
         return f.read()
 
 
