5c92bbd1db6da12156385a47bc49b7de4df151c5
Luis Felipe Castano <felipe.castano@edunext.co>
2023-06-13 17:43:33 -0500

    feat: add unit test

--- tests/distro/repository_validator/application/test_dpkg_url_validator.py
+++ tests/distro/repository_validator/application/test_dpkg_url_validator.py
@@ -1,17 +1,61 @@
 import pytest
-
+from tests.distro.repository_validator.infrastructure.in_memory_package_repository import InMemoryPackageRepository
 from tutordistro.distro.repository_validator.application.dpkg_url_validator import DPKGUrlValidator
+from tutordistro.distro.repository_validator.application.extra_pip_requirements_url_validator import ExtraPipRequirementsUrlValidator
+from tutordistro.distro.share.domain.package_does_not_exist import PackageDoesNotExist
 
-
+# Test case for validator when the repository exists
 def test_validator_when_repo_exists():
     # Given
-    repository = ""
+    repository = InMemoryPackageRepository(["https://github.com/test/testrepo/tree/2.0.1"])
     validator = DPKGUrlValidator(repository=repository)
 
     validator(
         name="ednx_package",
-        version="v1.0.0",
-
+        version="2.0.1",
+        domain="github.com",
+        extra={"path": "test",
+                "protocol": "https",
+                "repo": "testrepo"}
     )
 
-    #with PackageDoesNotExist:
+# Test case for validator when the repository does not exist
+def test_validator_when_repo_does_not_exists():
+    with pytest.raises(PackageDoesNotExist) as package_error:
+        # Given
+        repository = InMemoryPackageRepository([])
+        validator = DPKGUrlValidator(repository=repository)
+        # When
+        validator(
+            name="ednx_package",
+            version="2.0.1",
+            domain="github.com",
+            extra={"path": "test",
+                    "protocol": "https",
+                    "repo": "testrepo"}
+        )
+
+    # Then
+    assert package_error.type is PackageDoesNotExist
+
+# Test case for validator when extra pip requirements exist
+def test_validator_when_extra_pip_requeriments_exists():
+    #Given
+    repository = InMemoryPackageRepository(["https://github.com/eduNEXT/edx_xblock_scorm/tree/v2.0.0"])
+    validator = ExtraPipRequirementsUrlValidator(repository=repository)
+    validator(
+        url="git+https://github.com/eduNEXT/edx_xblock_scorm@v2.0.0#egg=scormxblock-xblock==2.0.0"
+    )
+
+# Test case for validator when extra pip requirements do not exist
+def test_validator_when_extra_pip_requeriments_does_not_exists():
+    with pytest.raises(PackageDoesNotExist) as package_error:
+        #Given
+        repository = InMemoryPackageRepository([])
+        validator = ExtraPipRequirementsUrlValidator(repository=repository)
+        # When
+        validator(
+            url="git+https://github.com/eduNEXT/edx_xblock_scorm@v2.0.0#egg=scormxblock-xblock==2.0.0"
+        )
+    # Then
+    assert package_error.type is PackageDoesNotExist
--- tests/distro/repository_validator/infrastructure/in_memory_package_repository.py
+++ tests/distro/repository_validator/infrastructure/in_memory_package_repository.py
@@ -4,10 +4,12 @@ from tutordistro.distro.share.domain.package_does_not_exist import PackageDoesNo
 
 
 class InMemoryPackageRepository(CloudPackageRepository):
-    repositories = [
-        "https://github.com/openedx/DoneXBlock/tree/2.0.1"
-    ]
+
+    def __init__(self, list_repositories: list) -> None:
+        self.repositories = list_repositories
+    
 
     def validate(self, package: CloudPackage) -> None:
         if not package.to_url() in self.repositories:
             raise PackageDoesNotExist(f"The package {package.name} or branch doesn't exist or is private")
+        
