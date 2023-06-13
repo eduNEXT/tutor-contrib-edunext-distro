35f03f59ba7b817e30d81866d9c5bfe751274e9c
Luis Felipe Castano <felipe.castano@edunext.co>
2023-06-13 15:11:53 -0500

    feat: add unit test

--- tests/distro/repository_validator/infrastructure/in_memory_package_repository.py
+++ tests/distro/repository_validator/infrastructure/in_memory_package_repository.py
@@ -1,12 +1,13 @@
-import requests
-
 from tutordistro.distro.share.domain.cloud_package import CloudPackage
 from tutordistro.distro.share.domain.cloud_package_repository import CloudPackageRepository
 from tutordistro.distro.share.domain.package_does_not_exist import PackageDoesNotExist
 
 
-class GitPackageRepository(CloudPackageRepository):
+class InMemoryPackageRepository(CloudPackageRepository):
+    repositories = [
+        "https://github.com/openedx/DoneXBlock/tree/2.0.1"
+    ]
+
     def validate(self, package: CloudPackage) -> None:
-        response = requests.get(package.to_url())
-        if response.status_code != 200:
+        if not package.to_url() in self.repositories:
             raise PackageDoesNotExist(f"The package {package.name} or branch doesn't exist or is private")
