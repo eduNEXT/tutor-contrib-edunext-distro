a456628f07b50bd33b8454ba7924f300bf8da668
Juan David Buitrago <juan.buitrago@edunext.co>
2023-06-14 10:46:49 -0500

    test: improve assert version test

--- tests/test_distro.py
+++ tests/test_distro.py
@@ -2,6 +2,8 @@
 Tutor distro plugin tests
 """
 
+import re
+
 from tutordistro.__about__ import __version__
 
 
@@ -9,4 +11,18 @@ def test_version():
     """
     Tutor distro plugin tests version
     """
-    assert __version__ == '15.0.2'
+    version = get_current_version()
+    assert __version__ == version
+
+
+def get_current_version():
+    """
+    Get the current version from 'setup.cfg'
+    """
+    with open('setup.cfg', 'r', encoding='utf-8') as f_setup:
+        file = f_setup.read()
+    pattern_version = re.escape("current_version = ") + r"\d+\.\d+\.\d+"
+    version = re.findall(pattern_version, file)
+    current_version = version[0].split("=")[1].split()
+
+    return current_version[0]
