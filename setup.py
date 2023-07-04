464b1e2f63935cc9eb682314173bed5a2ed57eb9
Juan David Buitrago <juan.buitrago@edunext.co>
2023-07-04 12:37:20 -0500

    fix: add schema requirement

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_packages(exclude=["tests*"]),
     include_package_data=True,
     python_requires=">=3.6",
-    install_requires=["tutor>=15.0.0, <16", "click"],
+    install_requires=["tutor>=15.0.0, <16", "click", "schema"],
     extras_require={
             "test": ["behave", "pytest", "pylint", "pytest-mock", "pycodestyle", "isort", "schema"]
         },
