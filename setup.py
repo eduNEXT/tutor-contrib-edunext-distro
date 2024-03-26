19ea4a5938262c80322fd0c55ab2973918ad798b
Andres Espinel <andressespinel@hotmail.com>
2024-03-26 13:38:01 -0500

    fix: updated minimun tutor version to fix private packages not installing properly

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     python_requires=">=3.8",
-    install_requires=["tutor>=17.0.0, <18", "click", "schema"],
+    install_requires=["tutor>=17.0.3, <18", "click", "schema"],
     extras_require={
             "test": ["behave", "pytest", "pylint", "pytest-mock", "pycodestyle", "isort", "schema"]
         },
